efa4c758ac1515c98986031134963b07fdad5b2c
Mariusz Ruczkowski <mario@Mariuszs-Mini.lan>
2023-03-05 19:10:57 +0000

    Add about section

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,17 +1,20 @@
 import React from "react";
+import "./About.css"
+import background3 from '../../images/child-1837375__340.webp'
+
+
 
 function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui
-        mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
-        porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam
-        semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed
-        rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+<>
+    <div className="container3">
+      <img src={background3} alt="logo"></img>
+      <h1 className="description">Hey there! My name is Mariusz, and I'm an aspiring frontend developer. I'm really passionate about creating beautiful and functional websites and applications that make a positive impact on people's lives.
+
+      I've been teaching myself frontend development for the last 4 months or so, and I've learned a lot about HTML, CSS, and JavaScript. I've also experimented with various frameworks and libraries, like React and Bootstrap, and I'm excited to continue exploring new technologies and techniques.
+      </h1>
     </div>
+    </>
   );
 }
 
