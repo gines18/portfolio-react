7b184850f0b4689e5d1067e22a5c4d123feecda6
Mariusz Ruczkowski <mario@Mariuszs-Mini.lan>
2023-03-06 20:09:24 +0000

    update Portfolio, add images and link to projects

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,39 +1,40 @@
 import React from "react";
 import Projects from "./PortfolioCard.js"
 
+
 const ProjectsData = [
   {
-    image: "https://via.placeholder.com/150",
+    image:  './images/semantic.jpg',
     title: "Improved semantic elements",
     description: "I have improved better accessibility, search engine optimization, maintainability, and improved browser rendering.",
     link: "https://github.com/gines18/Improved-semantic-elements",
   },
   {
-    image: "https://via.placeholder.com/150",
+    image: './images/HomeSection.jpg',
     title: "Portfolio",
     description: "This portfolio showing my ability to use CSS Grid Layout.",
     link: "https://github.com/gines18/NewPortfolio2022",
   },
   {
-    image: "https://via.placeholder.com/150",
+    image: './images/readme.jpg',
     title: "Readme generator",
     description: "Create your Readme without a hassle. Node.js, npm.",
     link: "https://github.com/gines18/Generate_Readme",
   },
   {
-    image: "https://via.placeholder.com/150",
+    image: './images/workSchedule.jpg',
     title: "Work Scheduling App",
     description: "Plan your work day with easy to use app.",
     link: "https://github.com/gines18/Job-Scheduling-App",
   },
   {
-    image: "https://via.placeholder.com/150",
+    image: './images/yourlocal.jpg',
     title: "YourLocal",
     description: "As a news-hungry individual who is always on the go, you want to have access to a website with an API that provides you with the latest local news, entertainment, and weather updates based on my current location.",
     link: "https://github.com/WloMac/YourLocal",
   },
   {
-    image: "https://via.placeholder.com/150",
+    image: './images/certificate.jpg',
     title: "My certificates",
     description: "In this Responsive Web Design Certification, I've learnt the languages that developers use to build webpages: HTML (Hypertext Markup Language) for content, and CSS (Cascading Style Sheets) for design.",
     link: "https://github.com/gines18/My-certificates",
@@ -42,9 +43,25 @@ const ProjectsData = [
 
 export default function Portfolio() {
 return (
+  
 <div className="Portfolio">
+
+<ul class="background">
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+</ul>
   {ProjectsData.map((project) => (
-<Projects title={project.title} image={project.image} description={project.description}/>
+<Projects title={project.title} image={project.image} description={project.description} link={project.link}/>
   ))}
   </div>
 );
--- src/components/pages/PortfolioCard.js
+++ src/components/pages/PortfolioCard.js
@@ -1,17 +1,21 @@
 import React from 'react'
-
+import "./Portfolio.css"
+import { Link } from 'react-router-dom';
 
       const Projects = (props) => {
-const { title, image, description} = props;
-console.log(props)
+const { title, image, description, link} = props;
+
 return (
+      <div className="card-container">
       <div className="card" style={{width: "18rem"}}>
   <img src={`${image}`} className="card-img-top" alt="laptop"></img>
   <div className="card-body">
     <h5 className="card-title">{`${title}`}</h5>
     <p className="card-text">{`${description}`}</p>
-    <a href="http://www.google.com" className="btn btn-primary">Open</a>
+    
+    <Link target="_blank" to = {`${link}`} ><button className="btn btn-primary">Open</button></Link>
   </div>
+</div>
 </div>
       )}
    
