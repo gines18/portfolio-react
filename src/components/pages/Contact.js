9fc5b432b4178972ec29fc2bc6ded307778a79b2
Mariusz Ruczkowski <mario@Mariuszs-Mini.lan>
2023-03-08 14:20:06 +0000

    Update CV and LinkedIn links

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -11,19 +11,16 @@ function Contact(props) {
     <div className='container4'>
 <img className="img1" src={background2} alt="contact"/>
     </div>
-
 <div className='container5'>
-
     <Link to="learn" role="button" className=" original-button text-decoration-none btn-link">
    Contact me
     </Link>
-
     <Routes>
     <Route path="learn" element={<Learn />} />
     </Routes>
-
 </div>
 <Footer />
+
     </>
   );
 }
--- src/components/pages/Learn.js
+++ src/components/pages/Learn.js
@@ -8,14 +8,14 @@ function Contact(props) {
   <>
 <div className='container5'>
 
-    <Link to="http://www.google.com" target="_blank" role="button" className=" original-button text-decoration-none btn-link">
+    <Link to="https://drive.google.com/file/d/1BG4mOlpGO58uN1uS4wHkVhoVsKtaPoqq/view?usp=sharing" target="_blank" role="button" className=" original-button text-decoration-none btn-link">
     PDF CV
     </Link>
     <Link to="https://github.com/gines18" target="_blank" role="button" className="original-button text-decoration-none btn-link">
    GitHub
     </Link>
-    <Link to="https://www.linkedin.com/in/mariusz-ruczkowski-824513203/" target="_blank" role="button" className="original-button text-decoration-none  btn-link">
-   LinkedLn
+    <Link to="https://www.linkedin.com/in/mariusz-ruczkowski" target="_blank" role="button" className="original-button text-decoration-none  btn-link">
+   LinkedIn
     </Link>
 
 </div>
