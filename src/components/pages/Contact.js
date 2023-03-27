e8c00fc5fdda72468a835a1ebc5bd24cad609022
gines18 <ruczkowski.m@gmail.com>
2023-03-27 17:16:06 +0100

    Add email in Contact section

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -10,10 +10,14 @@ function Contact(props) {
   <>
 <div class="card border-0">
 <h1 className="header">Crafting beautiful digital experiences, one pixel at a time.</h1>
+<div className="contact-container">
+<h1>E-mail:</h1>
+<p>ruczkowski.m@gmail.com</p>
+</div>
   <div class="card-body">
 <div className='container5'>
     <Link to="learn" role="button" className=" original-button text-decoration-none btn-link">
-   Contact me
+   Helpful links
     </Link>
     <Routes>
     <Route path="learn" element={<Learn />} />
@@ -21,6 +25,8 @@ function Contact(props) {
 </div>
   </div>
 </div>
+
+
     <div className='container4'>
 <img className="img1" src={background2} alt="contact"/>
     </div>
